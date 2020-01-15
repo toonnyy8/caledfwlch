71646d426be2cd60f06181c843f9181b163f3d81
toonnyy8 <toonnyy8@gmail.com>
2020-01-15 23:11:53 +0800

    update

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,8 +1,12 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
+import * as glm from "gl-matrix"
 import { glbDecoder, createShaderProgram } from "./gltool"
 import * as files from "../js/files"
 
+
+glm.glMatrix.setMatrixArrayType(Array)
+console.log(glm)
 let objs = Object.keys(files.glb).reduce((acc: object, curr: string) => {
     acc[curr] = glbDecoder(files.glb[curr])
     return acc
