78645d2c5453ab5318aea5d8a652f4566ac24e1a
toonnyy8 <toonnyy8@gmail.com>
2020-01-15 14:59:40 +0800

    update

--- src/js/files.js
+++ src/js/files.js
@@ -1,4 +1,3 @@
 import fs from "fs"
 
-export let actor = fs.readFileSync(__dirname + '/../../file/actor/actor.glb').buffer
 export let excalibur = fs.readFileSync(__dirname + '/../../file/actor/excalibur.glb').buffer
