2fc45d81b6d96689ef39a7af20093827acec5a91
toonnyy8 <toonnyy8@gmail.com>
2020-02-25 21:57:51 +0800

    update

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,9 @@
 import "core-js/stable"
 import "regenerator-runtime/runtime"
 import eruda from "eruda"
+import erudaDom from "eruda-dom"
 eruda.init();
+eruda.add(erudaDom)
 
 import * as glm from "../lib/gl-matrix/index"
 import { createShaderProgram } from "./gltool"
